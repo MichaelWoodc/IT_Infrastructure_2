a272cdf0a681c5f086c107eb964dea459a8d5770
Michael W. <michaelwoodcock@hotmail.com>
2024-09-16 21:07:34 -0400

    added code generating code

---
+++ slider_code.js
@@ -0,0 +1,297 @@
+
+<script>
+(() => {
+    // List of images with URLs
+    const images_list = [
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-00.jpeg",
+        "alt": "",
+        "name": "Step-00.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-01.jpeg",
+        "alt": "",
+        "name": "Step-01.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-02.jpeg",
+        "alt": "",
+        "name": "Step-02.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-03.jpeg",
+        "alt": "",
+        "name": "Step-03.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-04.jpeg",
+        "alt": "",
+        "name": "Step-04.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-05.jpeg",
+        "alt": "",
+        "name": "Step-05.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-06.jpeg",
+        "alt": "",
+        "name": "Step-06.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-07.jpeg",
+        "alt": "",
+        "name": "Step-07.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-08.jpeg",
+        "alt": "",
+        "name": "Step-08.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-09.jpeg",
+        "alt": "",
+        "name": "Step-09.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-10.jpeg",
+        "alt": "",
+        "name": "Step-10.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-11.jpeg",
+        "alt": "",
+        "name": "Step-11.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-12.jpeg",
+        "alt": "",
+        "name": "Step-12.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-13.jpeg",
+        "alt": "",
+        "name": "Step-13.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-14.jpeg",
+        "alt": "",
+        "name": "Step-14.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-15.jpeg",
+        "alt": "",
+        "name": "Step-15.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-16.jpeg",
+        "alt": "",
+        "name": "Step-16.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-17.jpeg",
+        "alt": "",
+        "name": "Step-17.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-18.jpeg",
+        "alt": "",
+        "name": "Step-18.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-19.jpeg",
+        "alt": "",
+        "name": "Step-19.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-20.jpeg",
+        "alt": "",
+        "name": "Step-20.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-21.jpeg",
+        "alt": "",
+        "name": "Step-21.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-22.jpeg",
+        "alt": "",
+        "name": "Step-22.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-23.jpeg",
+        "alt": "",
+        "name": "Step-23.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-24.jpeg",
+        "alt": "",
+        "name": "Step-24.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-25.jpeg",
+        "alt": "",
+        "name": "Step-25.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-26.jpeg",
+        "alt": "",
+        "name": "Step-26.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-27.jpeg",
+        "alt": "",
+        "name": "Step-27.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-28.jpeg",
+        "alt": "",
+        "name": "Step-28.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-29.jpeg",
+        "alt": "",
+        "name": "Step-29.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-30.jpeg",
+        "alt": "",
+        "name": "Step-30.jpeg",
+        "link": ""
+    },
+
+    {
+        "url": "https://michaelwoodc.github.io/IT_Infrastructure_2/3_install_joomla/Step-31.jpeg",
+        "alt": "",
+        "name": "Step-31.jpeg",
+        "link": ""
+    }
+    ];
+
+    // Generated by https://www.html-code-generator.com/html/image-slideshow-generator
+    let slider_id = document.querySelector("#hcg-slider-1");
+
+    // Append all images
+    let dots_div = "";
+    let images_div = "";
+    for (let i = 0; i < images_list.length; i++) {
+        // if no link without href="" tag
+        let href = (images_list[i].link == "" ? "":' href="'+images_list[i].link+'"');
+        images_div += '<a'+href+' class="hcg-slides animated"'+(i === 0 ? ' style="display:flex"':'')+'>'+
+                        '<span class="hcg-slide-number">'+(i+1)+'/'+images_list.length+'</span>'+
+                        '<img src="'+images_list[i].url+'" alt="'+images_list[i].name+'">'+
+                        '<span class="hcg-slide-text">'+images_list[i].name+'</span>'+
+                     '</a>';
+        dots_div += '<a href="#" class="hcg-slide-dot'+(i === 0 ? ' dot-active':'')+'" data-id="'+i+'"></a>';
+    }
+    slider_id.querySelector(".hcg-slider-body").innerHTML = images_div;
+    slider_id.querySelector(".hcg-slide-dot-control").innerHTML = dots_div;
+
+    let slide_index = 0;
+
+    const images = slider_id.querySelectorAll(".hcg-slides");
+    const dots = slider_id.querySelectorAll(".hcg-slide-dot");
+    const prev_button = slider_id.querySelector("#hcg-slide-prev");
+    const next_button = slider_id.querySelector("#hcg-slide-next");
+
+    const showSlides = () => {
+        if (slide_index > images.length-1) {
+            slide_index = 0;
+        }
+        if (slide_index < 0) {
+            slide_index = images.length-1;
+        }
+        for (let i = 0; i < images.length; i++) {
+            images[i].style.display = "none";
+            dots[i].classList.remove("dot-active");
+            if (i == slide_index) {
+                images[i].style.display = "flex";
+                dots[i].classList.add("dot-active");
+            }
+        }
+    }
+
+    prev_button.addEventListener("click", event => {
+        event.preventDefault();
+        slide_index--;
+        showSlides();
+    }, false);
+
+    next_button.addEventListener("click", event => {
+        event.preventDefault();
+        slide_index++;
+        showSlides();
+    }, false);
+
+    const dot_click = event => {
+        event.preventDefault();
+        slide_index = event.target.dataset.id;
+        showSlides();
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].addEventListener("click", dot_click, false);
+    }
+})();
+</script>
